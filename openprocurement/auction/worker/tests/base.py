75ba09dfe11abab6f0980b28d2ca7ac624b7bc54
dimon.obert <dimon.obert@quintagroup.org>
2017-06-22 13:55:59 +0300

    add db fixture to init test db

--- openprocurement/auction/worker/tests/base.py
+++ openprocurement/auction/worker/tests/base.py
@@ -2,21 +2,25 @@ import json
 import os
 import pytest
 import yaml
+import couchdb
 
 from openprocurement.auction.worker.auction import Auction
 
-PWD = os.path.dirname(os.path.realpath(__file__ ))
+PWD = os.path.dirname(os.path.realpath(__file__))
 # from openprocurement.auction.tests.main import update_auctionPeriod
+
+
+tender_file_path = os.path.join(PWD, "data/tender_data.json")
+worker_defaults_file_path = os.path.join(PWD, "data/auction_worker_defaults.yaml")
+with open(tender_file_path) as data_file:
+    tender_data = json.load(data_file)
+with open(worker_defaults_file_path) as stream:
+    worker_defaults = yaml.load(stream)
 
 
 @pytest.fixture(scope="module")
 def auction():
-    tender_file_path = os.path.join(PWD, "data/tender_data.json")
-    worker_defaults_file_path = os.path.join(PWD, "data/auction_worker_defaults.yaml")
     # update_auctionPeriod(tender_file_path)
-
-    with open(tender_file_path) as data_file:
-        tender_data = json.load(data_file)
 
     return Auction(
         tender_id=tender_data['data']['tenderID'],
@@ -24,3 +28,17 @@ def auction():
         auction_data=tender_data,
         lot_id=False
     )
+
+
+@pytest.fixture(scope='function')
+def db(request):
+    server = couchdb.Server("http://" + worker_defaults['COUCH_DATABASE'].split('/')[2])
+    name = worker_defaults['COUCH_DATABASE'].split('/')[3]
+
+    def delete():
+        del server[name]
+
+    if name in server:
+        delete()
+    server.create(name)
+    request.addfinalizer(delete)
