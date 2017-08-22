fab5d4834df2c5aa6e8f5742fcc4114581249415
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-08-22 15:06:19 +0300

    Fixed run

--- openprocurement/auction/worker/server.py
+++ openprocurement/auction/worker/server.py
@@ -219,7 +219,7 @@ def kickclient():
 
 
 def run_server(auction, mapping_expire_time, logger,
-               timezone='Europe/Kiev', bids_form=BidsForm, form_handler=form_handler, cookie_path='tenders'):
+               timezone='Europe/Kiev', bids_form=BidsForm, form_handler=form_handler, cookie_path='auctions'):
     app.config.update(auction.worker_defaults)
     # Replace Flask custom logger
     app.logger_name = logger.name
